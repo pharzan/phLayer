0086e3ed0aa2646e7f21d090efb4ca91e5f865fd
pharzan <pharzan@outlook.com>
2015-10-24 16:56:34 +0300

    QuestionLayer initial settings

--- QuenLayer.js
+++ QuenLayer.js
@@ -2,9 +2,19 @@
  * Created by Farzan on 10/24/2015.
  */
 var QuenLayer = {
+    config: [{
+        1: {
+            question: 'who is this?',
+            answers: ['pharzan', 'a big bunny', 'you'],
+            correct: [1]
+
+        }
+    }],
 
     view: function (ctrl, parent) {
+
         var self = this;
+
         return m('div', 'hello')
     }
 };
